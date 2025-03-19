1f0d1707d1fc0700523605a0d9156c4b18f0dc34
Alp Tahta <alp.tht48@gmail.com>
2025-03-19 10:53:04 +0300

    Add solutions for Exercise 1-1 and 1-2

---
+++ Chapter-01/Exercise-01-02/Exercise-01-02.c
@@ -0,0 +1,9 @@
+/*
+ * Exercise 1-2. Experiment to find out what happens when prints' arguments string contains \c, where c is some character not listed above.
+*/
+#include <stdio.h>
+
+main()
+{
+    printf("hello, world\c");
+}
