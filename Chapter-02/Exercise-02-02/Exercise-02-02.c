3efb981f5eb6ad7a0cf1917e51109184152cfdf4
Alp Tahta <alp.tht48@gmail.com>
2025-06-12 18:21:22 +0300

    Add solution for exercise 2.02

---
+++ Chapter-02/Exercise-02-02/Exercise-02-02.c
@@ -0,0 +1,55 @@
+/*
+ * Exercise 2-2. Write a loop equivalent to the for loop above without using &&
+ * or ||. 
+ *
+ */
+
+/*
+ * 		for (i=0; i < lim-1 && (c=getchar()) != '\n' && c != EOF; ++i)
+ * 			s[i] = c;
+ *
+ * 	NOTE: expressions connected by logical operators are evaluated left to
+ * 	right.
+*/
+#include <stdio.h>
+
+#define MAXLINE 1000         /* maximum input line length */
+
+/* functions */
+int getLine(char [], int);
+
+/* getLine function: read a line into s, return length */
+int getLine(char s[], int lim)
+{
+	int c, i, halt;
+
+	halt = 0;
+	for (i = 0; !halt; ++i) {
+		if (i > lim - 1)
+			halt = 1;
+		else if ((c = getchar()) == '\n')
+			halt = 1;
+		else if (c == EOF)
+			halt = 1;
+		else
+			s[i] = c;
+	}
+	--i;
+
+	if (c == '\n')
+		s[i++] = c;
+
+	s[i] = '\0';
+
+	return i;
+}
+
+int main(void)
+{
+	char line[MAXLINE];
+
+	while (getLine(line, MAXLINE) > 0)
+		printf("%s", line);
+
+	return 0;
+}
